8608ce712457c14aec2b8d894bc391da7c2d4f32
Jigar Tandel <tandel@sheridancollege.ca>
2025-02-01 17:35:37 -0500

    BookList.java Created

---
+++ src/main/java/sheridancollege/tandelj/onlinebookstore/model/BookList.java
@@ -0,0 +1,15 @@
+package sheridancollege.tandelj.onlinebookstore.model;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class BookList {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+}
