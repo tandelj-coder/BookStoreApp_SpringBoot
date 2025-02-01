8a5bea0d3bb70221b4e3dd257ffa73f78fa8afc5
Jigar Tandel <tandel@sheridancollege.ca>
2025-02-01 17:36:00 -0500

    BookCartList.java Created

---
+++ src/main/java/sheridancollege/tandelj/onlinebookstore/model/BookCartList.java
@@ -0,0 +1,19 @@
+package sheridancollege.tandelj.onlinebookstore.model;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class BookCartList {
+    private List<Book> cartItems = new ArrayList<>();
+
+    public void addToCart(Book book) {
+        cartItems.add(book);
+    }
+
+    public double calculateTotal() {
+        return cartItems.stream().mapToDouble(Book::getBookPrice).sum();
+    }
+}
